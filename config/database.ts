00b291760504271c67518e32d76af45acbdccd03
Alhouseny Camara <camaraalhouseny44@gmail.com>
2023-03-05 22:09:32 -0500

    initialize

---
+++ config/database.ts
@@ -0,0 +1,15 @@
+const config = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'quotly-strapi'),
+      user: env('DATABASE_USERNAME', 'postgres'),
+      password: env('DATABASE_PASSWORD', 'P@lymath444'),
+      ssl: env.bool('DATABASE_SSL', false),
+    },
+  },
+});
+
+export default config;
